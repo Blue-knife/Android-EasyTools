9e04831c6c0102506815b9109975e7dc8b6daf6f
345 <lv345_y@163.com>
2019-12-12 00:59:15 +0800

    添加一键式打开相机、相册，裁剪

---
+++ app/src/main/java/com/business/tools/ContextTools.java
@@ -0,0 +1,22 @@
+package com.business.tools;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Petterp
+ * on 2019-10-26
+ * Function:
+ */
+public class ContextTools extends Application {
+
+    public static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+    }
+
+}
---
+++ app/src/main/java/com/business/tools/camera/CameraActivity.java
@@ -0,0 +1,103 @@
+package com.business.tools.camera;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.app.ActivityCompat;
+
+import com.business.tools.ToastUtils;
+import com.business.toos.R;
+
+/**
+ * @author 345 QQ:1831712732
+ * @name Android Business Toos
+ * @class name：com.business.toos.camera
+ * @time 2019/12/11 23:02
+ * @description
+ */
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int REQUEST_CODE = 0x01;
+
+    public AppCompatImageView mImage;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        mImage = findViewById(R.id.activity_camera_image);
+        findViewById(R.id.activity_camera_start).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        checkPermission(new String[]{Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE});
+    }
+
+    public void checkPermission(String[] permission) {
+        if (Build.VERSION.SDK_INT < 23 || permission.length == 0) {
+            start();
+        } else {
+            requestPermissions(permission, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            for (int grantResult : grantResults) {
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "权限获取失败", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+            start();
+        }
+    }
+
+    public void start() {
+        ToolsCamera.start(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            //相机回调
+            case RequestCode.TAKE_PHOTO:
+                final Uri resultUri = CameraImageBean.getInstance().getPath();
+                CropPhoto.cropPhoto(this, true, resultUri);
+                break;
+            case RequestCode.PICK_PHOTO:
+                if (data != null) {
+                    final Uri pickPath = data.getData();
+                    CropPhoto.cropPhoto(this, false, pickPath);
+                }
+                break;
+            case RequestCode.CROP_PHOTO:
+                Bitmap bitmap = BitmapFactory.decodeFile(CameraImageBean.getInstance().getPath().getPath());
+                if (bitmap != null) {
+                    mImage.setImageBitmap(bitmap);
+                } else {
+                    Toast.makeText(this, "图片加载失败", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
