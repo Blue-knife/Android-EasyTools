bce4079d6a6876ed0524242be36d51a3b305c2ac
petterp <shiyihuicloud@163.com>
2022-01-10 17:15:53 +0800

    perf:优化系统分享工具

--- bullet_ktx/src/main/java/com/bullet/ktx/utils/SystemShareKtx.kt
+++ bullet_ktx/src/main/java/com/bullet/ktx/utils/SystemShareKtx.kt
@@ -13,8 +13,6 @@ import androidx.annotation.RequiresApi
 import androidx.core.content.FileProvider
 import com.bullet.ktx.file.FileKtxUtils
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
 
@@ -30,15 +28,15 @@ import java.io.File
 fun Context.isAlreadyInstalled(packageName: String): Boolean {
     val packageManager = packageManager
     try {
-        val packageInfo = packageManager.getPackageInfo(packageName, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT)
+        val packageInfo = packageManager.getPackageInfo(
+            packageName,
+            PackageManager.COMPONENT_ENABLED_STATE_DEFAULT
+        )
         if (packageInfo != null) {
             return true
         }
     } catch (e: PackageManager.NameNotFoundException) {
         e.printStackTrace()
-    }
-    GlobalScope.launch {
-
     }
     return false
 }
@@ -57,7 +55,8 @@ fun shareSinaText(context: Context, url: String, message: String) {
             action = Intent.ACTION_VIEW
             type = "text/plain"
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            component = ComponentName(sinaPageName, "com.sina.weibo.composerinde.ComposerDispatchActivity")
+            component =
+                ComponentName(sinaPageName, "com.sina.weibo.composerinde.ComposerDispatchActivity")
             putExtra(Intent.EXTRA_TEXT, "$url $message")
         }
         context.startActivity(intent)
@@ -83,7 +82,8 @@ suspend fun shareSinaImage(context: Context, filePath: String, bitmap: Bitmap?) 
             action = Intent.ACTION_SEND
             type = "image/*"
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            component = ComponentName(sinaPageName, "com.sina.weibo.composerinde.ComposerDispatchActivity")
+            component =
+                ComponentName(sinaPageName, "com.sina.weibo.composerinde.ComposerDispatchActivity")
             if (filePath.isEmpty()) {
                 if (bitmap == null) {
                     withContext(Dispatchers.Main) {
@@ -99,7 +99,12 @@ suspend fun shareSinaImage(context: Context, filePath: String, bitmap: Bitmap?) 
                 val file = File(filePath)
                 when {
                     Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
-                        val uri = FileKtxUtils.saveFileToDownload(file, file.name, mimeType = "image/*", folderPath = "Pictures/lanfan")
+                        val uri = FileKtxUtils.saveFileToDownload(
+                            file,
+                            file.name,
+                            mimeType = "image/*",
+                            folderPath = "Pictures/lanfan"
+                        )
                         putExtra(Intent.EXTRA_STREAM, uri)
                     }
                     Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> {
